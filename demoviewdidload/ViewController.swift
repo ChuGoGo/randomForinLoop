c768986696b18bc930817a3724e035c6152b0df3
chugogo <ioueasonae86@yahoo.com.tw>
2022-03-21 21:06:57 +0800

    Initial Commit

---
+++ demoviewdidload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoviewdidload
+//
+//  Created by Chu Go-Go on 2022/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
